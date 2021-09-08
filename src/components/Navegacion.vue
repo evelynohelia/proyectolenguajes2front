01f6fce372907589a0518e8c3909789226cee71b
wasantac <34143200+wasantac@users.noreply.github.com>
2021-09-08 11:13:37 -0500

    Validacion Login

--- src/components/Navegacion.vue
+++ src/components/Navegacion.vue
@@ -3,6 +3,12 @@
         <v-btn href="/" color="yellow" elevation="0">
         <v-toolbar-title class="headline">Schedule</v-toolbar-title>
         </v-btn>
+        <v-btn v-bind:href="'/historial/' + id" color="yellow" elevation="0">
+        <v-toolbar-title class="headline">Historial</v-toolbar-title>
+        </v-btn>
+        <v-btn v-if="prof" v-bind:href="'/citasPendientes/' + id" color="yellow" elevation="0">
+        <v-toolbar-title class="headline">Citas Pendientes</v-toolbar-title>
+        </v-btn>
         <v-spacer/>
         <v-btn href="/profile" color="yellow" elevation="0">
         <div class="headline d-flex align-center">
@@ -19,10 +25,43 @@
 </template>
 
 <script>
+import axios from 'axios';
 export default {
     name: 'Navegacion',
     data (){
-        return {}
+        return {
+            prof:false,
+            id: 0,
+        }
+    },
+    mounted(){
+        let token = localStorage.getItem('token',token);
+        if(token === null){
+            this.$router.push('login')
+        }else{
+            axios.get('http://localhost:8000/api/personas?token='+token, {headers: {'X-Requested-With': 'XMLHttpRequest'}})
+            .then((response) =>{
+                if(response.status!=200) {
+                  this.$router.push('login')
+
+                }
+            })
+            .catch(function(err) {
+                console.log(err);
+            })     
+        }
+        let base64URL = token.split('.')[1];
+
+        let base64 = base64URL.replace('-','+').replace('_','/')
+        let decrypt = JSON.parse(window.atob(base64));
+        this.id = decrypt.sub;
+        axios.get(`http://localhost:8000/api/personaProfesional/${decrypt.sub}`).then(res => {
+            if(res.data.length != 0){
+                this.prof= true;
+            }
+        }).catch(function(err) {
+                console.log(err);
+        })  
     }
 }
 </script>
